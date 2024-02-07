50dbc0b5419af27819152f547a5ba08f7097f09f
bashiryousufy <bashiryousufy@gmail.com>
2024-02-08 07:19:49 +0800

    remove placeholder

--- src/pages/Categories/CreateEditCategoryForm.tsx
+++ src/pages/Categories/CreateEditCategoryForm.tsx
@@ -68,7 +68,7 @@ const CreateEditCategoryForm: React.FC = () => {
 
                         <div className="mt-4">
                             <span className="font-light text-xs mb-1">Category Name</span>
-                            <input value={category?.name} type="text" className=" border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="John" required />
+                            <input value={category?.name} type="text" className=" border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="" required />
                         </div>
 
                         <div className="mt-4">
